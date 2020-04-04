af247a3340cf4484b94d6c7e9ab7ecf514cf82bf
Qingyao Sun <sunqingyao19970825@icloud.com>
2020-04-03 22:37:31 -0600

    Initial commit

---
+++ app.py
@@ -0,0 +1,67 @@
+import binascii
+from datetime import datetime
+import os
+import re
+import pathlib
+from flask import Flask, render_template, flash, request, redirect, url_for
+from werkzeug.utils import secure_filename
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+app = Flask(__name__)
+
+app.config.update(
+    SECRET_KEY=os.environ['FLASK_SECRET_KEY'],
+    UPLOAD_FOLDER=os.path.join(basedir, 'uploads'),
+    MAX_CONTENT_LENGTH=16 * 1024 * 1024    # set maximum file size to 16 MiB
+)
+
+sid_validator = re.compile('^[0-9]{11}$', re.ASCII)
+
+
+@app.route('/', methods=['GET', 'POST'])
+def handle_form():
+    if request.method == 'POST':
+        if not request.form.get('student-id'):
+            flash('No student ID')
+            return redirect(request.url)
+        sid = request.form.get('student-id')
+
+        if not sid_validator.fullmatch(sid):
+            flash('Student ID is not an 11-digit number')
+            return redirect(request.url)
+
+        # check if the post request has the file part
+        if 'submission' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['submission']
+
+        # if user does not select file, browser also
+        # submit an empty part without filename
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+
+        personal_folder = os.path.join(
+            app.config['UPLOAD_FOLDER'],
+            datetime.today().strftime('%Y-%m-%d'),
+            sid
+        )
+
+        pathlib.Path(personal_folder).mkdir(parents=True, exist_ok=True)
+        filename = secure_filename(file.filename)
+        file.save(os.path.join(personal_folder, filename))
+
+        file.seek(0)
+        checksum = binascii.crc32(file.read())
+
+        flash(f'CRC32({filename[:20] + (filename[20:] and "[...]")}) = {checksum & 0xFFFFFFFF:#010X}.')
+
+        return redirect(url_for('handle_form'))
+
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
